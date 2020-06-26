1165ad015d9809622f836c76df67437d086af683
Bruno Garcia <bruno@garciaechegaray.com>
2020-06-26 18:49:45 +0200

    feat: Create the database models for actors and movies

--- models.py
+++ models.py
@@ -1,5 +1,13 @@
-from sqlalchemy import Column, String, Integer
+# ----------------------------------------------------------------------------#
+# Imports
+# ----------------------------------------------------------------------------#
+
+from sqlalchemy import Column, String, Integer, DateTime
 from flask_sqlalchemy import SQLAlchemy
+
+# ----------------------------------------------------------------------------#
+# App Config.
+# ----------------------------------------------------------------------------#
 
 user = "postgres"
 pwd = 'postgres'
@@ -23,3 +31,78 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
     db.create_all()
+
+# ----------------------------------------------------------------------------#
+# Models.
+# ----------------------------------------------------------------------------#
+
+
+class Movies(db.Model):
+    '''
+    Movies
+
+    '''
+    __tablename__ = 'movies'
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  release_date = Column(DateTime)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+          'id': self.id,
+          'title': self.title,
+          'release_date': self.release_date,
+        }
+
+
+class Actors(db.Model):
+    '''
+    Actors
+
+    '''
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(String)
+    gender = Column(String)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+          'id': self.id,
+          'name': self.name,
+          'age': self.age,
+          'gender': self.gender,
+        }
